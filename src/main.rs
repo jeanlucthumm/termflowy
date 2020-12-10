32b5da222e6fcc51a4c863014f60200d6d4d2903
jeanlucthumm <jeanlucthumm@gmail.com>
2020-12-10 01:40:59 -0800

    Remove old test functions

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,10 @@ use std::char;
 mod editor;
 mod render;
 mod tree;
+mod raster;
 
 use editor::Editor;
 use render::debug;
-
-fn test1() {
-    n::addstr("Type any character to see it in bold\n");
-    let ch = n::getch();
-
-    if ch == n::KEY_F(1) {
-        n::addstr("F1 key pressed");
-    } else {
-        n::addstr("The pressed key is ");
-        n::attron(n::A_BOLD() | n::A_BLINK());
-        n::addstr(&format!(
-            "{}\n",
-            char::from_u32(ch as u32).expect("could not convert character")
-        ));
-        n::attroff(n::A_BOLD() | n::A_BLINK());
-    }
-}
-
-fn print_center(msg: &str) {
-    let (y, x) = render::get_max_yx(n::stdscr());
-    n::mvprintw(y / 2, (x - msg.chars().count() as i32) / 2, msg);
-}
 
 fn main_loop(e: &mut Editor, windows: render::WindowStore) {
     e.init();
