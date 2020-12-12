42a378e0d0dc3beeb027dad6ec406fbfc42cfc16
jeanlucthumm <jeanlucthumm@gmail.com>
2020-12-11 17:33:52 -0800

    Add quick latency tracker for key presses so I don't bog down the loop too much

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 #![allow(dead_code)]
+use editor::Editor;
 use ncurses as n;
+use render::debug;
+use std::time::{Duration, Instant};
 
 mod editor;
+mod raster;
 mod render;
 mod tree;
-mod raster;
 
-use editor::Editor;
-use render::debug;
+struct RenderStats {
+    key_render_times: Vec<Duration>,
+}
 
-fn main_loop(e: &mut Editor, windows: render::WindowStore) {
+fn average(times: &Vec<Duration>) -> f32 {
+    times.iter().map(|d| d.as_millis()).sum::<u128>() as f32 / times.len() as f32
+}
+
+fn main_loop(e: &mut Editor, windows: render::WindowStore) -> RenderStats {
     e.init();
     let mut is_debug = false;
+    let mut stats = RenderStats {
+        key_render_times: vec![],
+    };
     loop {
         let key = n::getch();
         let key_str = n::keyname(key).unwrap();
@@ -24,13 +35,16 @@ fn main_loop(e: &mut Editor, windows: render::WindowStore) {
         if is_debug {
             debug::pprint(windows.debug, key_str);
         } else {
+            let now = Instant::now();
             if !e.on_key_press(key) {
                 break;
             }
+            stats.key_render_times.push(now.elapsed());
         }
 
         n::refresh();
     }
+    stats
 }
 
 fn main() {
@@ -40,8 +54,10 @@ fn main() {
         debug: render::debug::create_window(10, 50, 10, 10),
     };
 
-    main_loop(&mut Editor::new(n::stdscr()), window_store);
+    let stats = main_loop(&mut Editor::new(n::stdscr()), window_store);
 
     n::getch();
     n::endwin();
+
+    println!("average latency: {:.2}", average(&stats.key_render_times));
 }
