8ce4baa21b77b1fe6aec246c545073f23a2a8bb8
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2020-12-21 22:41:53 -0800

    command b

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
 
+use crate::editor;
 use crate::editor::Cursor::*;
 use crate::editor::{CommandState, Cursor, InsertState};
+use crate::raster::PixelState::*;
 use crate::raster::{Browser, Direction, PixelState, Raster};
 use crate::render::Point;
 use crate::tree::Tree;
-use crate::editor;
 
 pub fn new_command_map() -> HashMap<String, editor::CommandHandler> {
     let mut map: HashMap<String, editor::CommandHandler> = HashMap::new();
@@ -14,6 +15,7 @@ pub fn new_command_map() -> HashMap<String, editor::CommandHandler> {
     map.insert(String::from("l"), command_hl);
     map.insert(String::from("j"), command_jk);
     map.insert(String::from("k"), command_jk);
+    map.insert(String::from("b"), command_b);
     map
 }
 
@@ -73,7 +75,43 @@ pub fn command_jk(
                 .expect("y pos should never be bigger than col"),
         )?
         .map(|b| find_left_text(b, cursor.pos.1 as u32))?;
-    Ok(Command(CommandState { pos, col: cursor.col }))
+    Ok(Command(CommandState {
+        pos,
+        col: cursor.col,
+    }))
+}
+
+pub fn command_b(
+    _key: &str,
+    cursor: CommandState,
+    tree: &mut Tree,
+    raster: &Raster,
+) -> Result<Cursor, String> {
+    if let Text { id, offset } = raster.get(cursor.pos).unwrap() {
+        tree.activate(id)?;
+        let content_chars: Vec<char> = tree.get_active_content().chars().collect();
+        let mut new_offset = offset;
+        while new_offset > 0 {
+            new_offset -= 1;
+            if content_chars[new_offset] == ' ' && new_offset + 1 != offset {
+                new_offset += 1;
+                break;
+            }
+        }
+        let pos = raster
+            .browser(cursor.pos)
+            .unwrap()
+            .go_until_count(Direction::Left, (offset - new_offset) as u32, |state| {
+                state.is_text()
+            })?
+            .pos();
+        Ok(Command(CommandState { pos, col: pos.1 }))
+    } else {
+        Err(format!(
+            "pixel state at position {:?} should have been text",
+            cursor.pos
+        ))
+    }
 }
 
 fn find_left_text(b: Browser, col: u32) -> Result<Point, String> {
